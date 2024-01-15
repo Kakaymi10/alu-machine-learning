6416386f820d1c32d9569d959b21503d1fb49fd7
Kakaymi10 <m.moussa@alustudent.com>
2024-01-15 05:31:36 -0800

    Sahpe a matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 '''Write a function def matrix_shape(matrix): that calculates the shape of a matrix:
 
 You can assume all elements in the same dimension are of the same type/shape
