8d6426dfe3a010b30d600dcd52935e0b7fca4fff
Kakaymi10 <m.moussa@alustudent.com>
2024-01-16 04:37:51 -0800

    Transposition of a 2D matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-'''Write a function def matrix_shape(matrix): that calculates the shape of a matrix:
-You can assume all elements in the same dimension are of the same type/shape
-The shape should be returned as a list of integers '''
-=======
 '''
 Write a function def matrix_shape(matrix):
 that calculates the shape of a matrix:
@@ -12,7 +7,6 @@ dimension are of the same type/shape
 The shape should be returned as a list of integers
 '''
 
->>>>>>> 18924bc4c444d0b61653d9a8a7b8e070d3adde80
 
 def matrix_shape(matrix):
     '''
