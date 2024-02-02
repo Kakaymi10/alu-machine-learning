a0162292f745f9c2dcf248a472377343c7a03aa9
Kakaymi10 <m.moussa@alustudent.com>
2024-02-02 13:23:10 -0800

    Integral

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -12,8 +12,9 @@ def poly_integral(poly, C=0):
         C (int): Constant of integration.
 
     Returns:
-        list or None: List of coefficients representing the integral of the polynomial.
-                      Returns None if input is not a non-empty list or C is not an integer.
+        list or None: List of coefficients representing
+the integral of the polynomial.
+        Returns None if input is not a non-empty list or C is not an integer.
     """
     # Check for the validity of the inputs
     if not isinstance(poly, list) or not poly or not isinstance(C, int):
@@ -21,14 +22,14 @@ def poly_integral(poly, C=0):
 
     # Calculate the coefficients of the integral using the power rule
     integral = [poly[i] / (i + 1) for i in range(len(poly) - 1, 0, -1)]
-    integral.append(poly[0] / 1)  # The integral of a constant term is the term itself
+    integral.append(poly[0] / 1)
     integral.append(C)
 
-    # If the original polynomial is a constant term (0), set the integral to [C]
+    # If the original polynomial is a constant term (0), set the intgl to [C]
     if len(poly) == 1 and poly[0] == 0:
         integral = [C]
 
-    # Convert any floating-point coefficients to integers if they are whole numbers
+    # Convert any floating-point coefficients to integers if they are ws
     integral = [int(coeff) if coeff % 1 == 0 else coeff for coeff in integral]
 
     return integral[::-1]
