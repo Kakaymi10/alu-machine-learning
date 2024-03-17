44f16a54fbb0d740ea54f7ff872b19d7864cba3f
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-03-17 22:28:44 +0200

    Update 100-continuous.py

--- math/bayesian_prob/100-continuous.py
+++ math/bayesian_prob/100-continuous.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
-"""Compute the posterior probability that the probability
-of experiencing severe side effects
-falls within a specified range, based on the given data."""
+"""Compute the posterior probability that the
+probability of experiencing
+severe side effects falls within
+a specified range, given the data.
+"""
+
+
 from scipy import special
 
 
 def posterior(x, n, p1, p2):
-    """Return the posterior probability that p is within the range
-    [p1, p2] given x and n."""
-    if not isinstance(n, int) or n <= 0:
+    """Return the posterior probability that p is within the range [p1, p2]
+       given x and n.
+    """
+    if type(n) is not int or n <= 0:
         raise ValueError('n must be a positive integer')
-    if not isinstance(x, int) or x < 0:
-        raise ValueError('x must be a non-negative integer')
+    if type(x) is not int or x < 0:
+        text = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(text)
     if x > n:
         raise ValueError('x cannot be greater than n')
-    if not isinstance(p1, float) or p1 < 0 or p1 > 1:
+    if type(p1) is not float or p1 < 0 or p1 > 1:
         raise ValueError('p1 must be a float in the range [0, 1]')
-    if not isinstance(p2, float) or p2 < 0 or p2 > 1:
+    if type(p2) is not float or p2 < 0 or p2 > 1:
         raise ValueError('p2 must be a float in the range [0, 1]')
     if p2 <= p1:
         raise ValueError('p2 must be greater than p1')
-    a = special.btdtr(x + 1, n - x + 1, p1)
-    b = special.btdtr(x + 1, n - x + 1, p2)
-    return a - b
+    return special.btdtr(x+1, n-x+1, p2) - special.btdtr(x+1, n-x+1, p1)
