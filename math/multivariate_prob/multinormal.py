d0a8d42d8209ccd374db8afc0f7460fd61dad0df
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-03-17 15:22:21 +0200

    Update multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -16,15 +16,17 @@ class MultiNormal:
         Initialize the MultiNormal instance.
 
         Args:
-            data (numpy.ndarray): The input dataset of shape (d, n), where n is the number of data points
-                                  and d is the number of dimensions in each data point.
+            data (numpy.ndarray): The input dataset of shape (d, n),
+            where n is the number of data points
+                                  and d is the number
+                                  of dimensions in each data point.
         """
         if not isinstance(data, np.ndarray) or data.ndim != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
         d, n = data.shape
         if n < 2:
             raise ValueError("data must contain multiple data points")
-        
+
         self.mean = np.mean(data, axis=1, keepdims=True)
         centered_data = data - self.mean
         self.cov = np.dot(centered_data, centered_data.T) / (n - 1)
@@ -34,7 +36,8 @@ class MultiNormal:
         Calculate the Probability Density Function (PDF) at a data point.
 
         Args:
-            x (numpy.ndarray): The data point of shape (d, 1), where d is the number of dimensions.
+            x (numpy.ndarray): The data point of shape (d, 1), where
+            d is the number of dimensions.
 
         Returns:
             float: The value of the PDF.
@@ -46,7 +49,8 @@ class MultiNormal:
 
         d = self.mean.shape[0]
         normalization = 1 / np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))
-        exponent = -0.5 * np.dot(np.dot((x - self.mean).T, np.linalg.inv(self.cov)), (x - self.mean))
+        exponent = -0.5 * np.dot(np.dot((x - self.mean).T,
+                                        np.linalg.inv(self.cov)), (x - self.mean))
         pdf_value = normalization * np.exp(exponent)
 
         return pdf_value
