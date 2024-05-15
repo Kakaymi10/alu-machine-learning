33989f1c9ab97cbbdcaeba112ce1ec2e19496cda
Kakaymi1o <m.moussa@alustudent.con>
2024-05-15 08:27:10 +0200

    privatize instance attributes

--- supervised_learning/classification/9-neural_netwrok.py
+++ supervised_learning/classification/9-neural_netwrok.py
@@ -37,23 +37,18 @@ class NeuralNetwork:
     @property
     def W1(self):
         return self.__W1
-
     @property
     def b1(self):
         return self.__b1
-
     @property
     def A1(self):
         return self.__A1
-
     @property
     def W2(self):
         return self.__W2
-
     @property
     def b2(self):
         return self.__b2
-
     @property
     def A2(self):
         return self.__A2
