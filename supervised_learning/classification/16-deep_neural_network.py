2d1c5d0bdee99f9a04bf02d2516bfca758f4a8d8
Kakaymi1o <m.moussa@alustudent.con>
2024-05-18 10:02:02 +0200

    Initialize deep neural network

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -18,22 +18,25 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if type(layers) != list:
             raise TypeError('layers must be a list of positive integers')
-        if not layers:
-            raise ValueError('layers must be a list of positive integers')
-        self.nx = nx
-        self.layers = layers
-        self.L = len(layers)  # Number of layers
-        self.cache = {}  # Dictionary to store the values of the nodes
-        self.weights = {}  # Dictionary to store the weights and biases
+        if len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
 
-        # Initialize weights and biases
-        for l in range(self.L):
-            if not isinstance(layers[l], int) or layers[l] <= 0:
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
                 raise TypeError('layers must be a list of positive integers')
 
-            layer_input_size = nx if l == 0 else layers[l - 1]
-            weight_key = 'W' + str(l + 1)
-            weights_initializer = np.random.randn(layers[l], layer_input_size)
-            scaling_factor = np.sqrt(2. / layer_input_size)
-            self.weights[weight_key] = weights_initializer * scaling_factor
-            self.weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
+            if i == 0:
+                # He et al. initialization
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                # He et al. initialization
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+            # Zero initialization
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
