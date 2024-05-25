f940af272606fdd3fd38d8de09d592f1aca8cfcf
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-05-25 16:57:37 +0200

    Update 1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -12,10 +12,7 @@ def create_layer(prev, n, activation):
     Creates a layer for a neural network
     """
     # Initialize the weights using He et al. initialization
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-
-    # Create the dense layer with the specified number of nodes an actfunc
-    layer = tf.layers.dense(inputs=prev, units=n, activation=activation,
-                            kernel_initializer=initializer, name='layer')
-    
-    return layer
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    L = tf.layers.Dense(units=n, activation=activation,
+                        kernel_initializer=W, name="layer")
+    return L(prev)
