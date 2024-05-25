33e8ea4de5248b98bf056567819036435b98e18a
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-05-25 17:17:35 +0200

    Update 3-calculate_accuracy.py

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ calculate accuracy of neural network"""
-  
+
     p_m = tf.arg_max(y_pred, 1)
     y_m = tf.arg_max(y, 1)
     e = tf.equal(y_m, p_m)
