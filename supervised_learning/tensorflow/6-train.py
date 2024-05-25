8a1d7333e65580ed0bc449ba0c4c33d989af229e
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-05-25 17:16:51 +0200

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -3,15 +3,14 @@
 
 
 import tensorflow as tf
-from importlib import import_module
 
 
 # Import required functions
-calculate_accuracy = import_module('3-calculate_accuracy').calculate_accuracy
-calculate_loss = import_module('4-calculate_loss').calculate_loss
-create_placeholders = import_module('0-create_placeholders').create_placeholders
-create_train_op = import_module('5-create_train_op').create_train_op
-forward_prop = import_module('2-forward_prop').forward_prop
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
