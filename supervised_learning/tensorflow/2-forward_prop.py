771268a2fba8bbf12d1d50a6760bcadd4af4bb13
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-05-25 17:20:20 +0200

    Update 2-forward_prop.py

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -7,16 +7,10 @@ Neural Class
 import tensorflow as tf
 create_layer = __import__('1-create_layer').create_layer
 
-
 def forward_prop(x, layer_sizes=[], activations=[]):
-    '''
-    creates the forward propagation graph for the nueral network.
-    '''
-    # input data
+    """creates the forward propagation graph for the nueral network."""
     L = x
-
-    # loop thorugh the layers and create each one
     for i in range(len(layer_sizes)):
-        outout = create_layer(L, layer_sizes[i], activations[i])
-
+        L = create_layer(L, layer_sizes[i],
+                         activations[i])
     return L
