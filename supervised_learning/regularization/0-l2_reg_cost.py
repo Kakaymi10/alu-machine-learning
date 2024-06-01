7a63d3f380386dbe8f2a32c0df51256dcccbd138
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-06-01 20:53:06 +0200

    Update 0-l2_reg_cost.py

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -12,7 +12,9 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     '''
     regularizer = 0
     for i in range(1, L+1):
-        w = weights["W"+str(i)]
+        key = 'W' + str(i)
+        w = weights[key]
         regularizer += np.sum(np.square(w))
+    regularizer *= (lambtha / (2 * m))
     new_cost = cost + regularizer
     return new_cost
