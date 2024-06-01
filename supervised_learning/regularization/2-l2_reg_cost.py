5cb7f031418dae0cbb22be9b5caf75256efe1dd1
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2024-06-01 21:24:26 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -11,7 +11,7 @@ def l2_reg_cost(cost):
     """
     Calculates the cost of a neural network with L2 regularization
     """
-   
+
     l2_cost = cost + tf.losses.get_regularization_losses()
-    
+
     return l2_cost
