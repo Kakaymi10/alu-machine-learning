2e6ed1595fcb87c4ab8cd3414721f20ef2d4be07
Kakaymi10 <m.moussa@alustudent.com>
2024-06-27 08:20:50 -0700

    unpack paginations

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+triblizer in web scrapping
+'''
+
+
+import requests
+
+
+def sentientPlanets():
+    url = 'https://swapi-api.alx-tools.com/api/species/?page=1'
+    planets = []
+
+    while url:
+        response = requests.get(url).json()
+
+        for specy in response['results']:
+            if specy['designation'] == 'sentient':
+                if specy['homeworld']:
+                    get_planet = requests.get(specy['homeworld']).json()
+                    planets.append(get_planet['name'])
+        
+        url = response['next']
+    
+    return planets
