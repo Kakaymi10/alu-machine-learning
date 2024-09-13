1a5f0d625192846185ee6da3b6a3132208e2516e
Kakaymi10 <m.moussa@alustudent.com>
2024-09-13 19:26:24 +0200

    yay my first rnn cell run succesfully with error

---
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+RNN Cell
+'''
+
+
+import numpy as np
+import tensorflow as tf
+
+
+class RNNCell:
+    def __init__(self, i, h, o):
+        ''' Class constructor '''
+        self.Wy = np.random.randn(h, o)
+        self.by = np.zeros((1, o))
+        self.Wh = np.random.randn(i + h, h)
+        self.bh = np.zeros((1, h))
+
+    def forward(self, h_prev, x_t):
+        ''' Method that performs forward propagation for one time step '''
+        h_next = np.tanh(np.dot(np.hstack((h_prev, x_t)), self.Wh) + self.bh)
+        y = np.dot(h_next, self.Wy) + self.by
+        y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
+        return h_next, y
