1a3c5e172cc52fabeb39c423a6441b86a158f4b8
Kakaymi10 <m.moussa@alustudent.com>
2024-09-14 08:57:53 +0200

    fix error ,hopefully for the last time

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -12,15 +12,26 @@ def bi_rnn(bi_cell, X, h_0, h_t):
     Function that performs forward propagation for a bidirectional RNN
     '''
     t, m, i = X.shape
-    h = h_0.shape[2]
+    h = h_0.shape[1]
+
     Hf = np.zeros((t, m, h))
     Hb = np.zeros((t, m, h))
+
     Hf[0] = h_0
     Hb[-1] = h_t
-    for i in range(t):
+
+    # Forward pass
+    for i in range(1, t):
         Hf[i] = bi_cell.forward(Hf[i - 1], X[i])
-    for i in range(t - 1, -1, -1):
+
+    # Backward pass
+    for i in range(t - 2, -1, -1):
         Hb[i] = bi_cell.backward(Hb[i + 1], X[i])
+
+    # Concatenate forward and backward hidden states
     H = np.hstack((Hf, Hb))
+
+    # Output computation
     Y = bi_cell.output(H)
+
     return H, Y
