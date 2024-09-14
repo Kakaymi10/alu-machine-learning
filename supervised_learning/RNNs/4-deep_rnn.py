9162eb6d1911d1c6dcd25a3e6101e254b490e5f5
Kakaymi10 <m.moussa@alustudent.com>
2024-09-14 08:44:03 +0200

    confusing 1 and l

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -19,7 +19,7 @@ def deep_rnn(rnn_cells, X, h_0):
     H[0] = h_0
     for i in range(t):
         h_prev = X[i]
-        for j in range(l):
+        for j in range(num_layers):
             h_prev, y = rnn_cells[j].forward(H[i, j], h_prev)
             H[i + 1, j] = h_prev
         Y.append(y)
