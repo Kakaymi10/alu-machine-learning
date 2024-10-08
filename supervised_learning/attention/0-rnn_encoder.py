1b8bd9e5c894be9cf429ddae35372cd46a09dd46
Kakaymi10 <m.moussa@alustudent.com>
2024-10-08 15:34:30 +0200

    create rnn encoder class

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 '''
-Create a class RNNEncoder that inherits from tensorflow.keras.layers.Layer to encode for machine translation
+Create a class RNNEncoder that
+inherits from tensorflow.keras.layers.Layer
+to encode for machine translation
 '''
 
 
@@ -9,20 +11,24 @@ import tensorflow as tf
 
 class RNNEncoder(tf.keras.layers.Layer):
     '''
-    Class that inherits from tensorflow.keras.layers.Layer to encode for machine translation
+    Class that inherits from tensorflow.keras.layers.Layer
+    to encode for machine translation
     '''
     def __init__(self, vocab, embedding, units, batch):
         '''
         Class constructor
         '''
+        super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-        self.gru = tf.keras.layers.GRU(units,
-                                        return_sequences=True,
-                                        return_state=True,
-                                        recurrent_initializer='glorot_uniform')
-        
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+
     def initialize_hidden_state(self):
         '''
         Initializes the hidden states for the GRU
