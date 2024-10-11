0132114e97be94862a7a1e3e1a980a22494815bd
Kakaymi10 <m.moussa@alustudent.com>
2024-10-11 09:19:49 +0200

    self-attention

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -27,14 +27,14 @@ class SelfAttention(tf.keras.layers.Layer):
         '''
         # Expand s_prev to match hidden_states time steps
         s_prev_expanded = tf.expand_dims(s_prev, 1)
-        
+
         # Compute energies
         e = self.V(tf.nn.tanh(self.W(s_prev_expanded) + self.U(hidden_states)))
-        
+
         # Compute attention weights
         weights = tf.nn.softmax(e, axis=1)
-        
+
         # Compute the context vector
         context = tf.reduce_sum(weights * hidden_states, axis=1)
-        
+
         return context, weights
