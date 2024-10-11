c4b1903bd771f95efe4162a8599db21a943e393a
Kakaymi10 <m.moussa@alustudent.com>
2024-10-11 08:57:54 +0200

    self-attention

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -5,6 +5,7 @@ Module that contains the class SelfAttention
 
 
 import tensorflow as tf
+import numpy as np
 
 
 class SelfAttention(tf.keras.layers.Layer):
@@ -26,11 +27,14 @@ class SelfAttention(tf.keras.layers.Layer):
         '''
         Method that performs the self-attention
         '''
-        # Remove concatenation of s_prev and hidden_states
-        # s_prev = tf.expand_dims(s_prev, 1)
+        # Expand s_prev to match hidden_states time steps
+        s_prev_expanded = tf.expand_dims(s_prev, 1)
+        
+        # Broadcast s_prev to all time steps
+        s_prev_tiled = tf.tile(s_prev_expanded, [1, tf.shape(hidden_states)[1], 1])
         
         # Compute energies
-        e = self.V(tf.nn.tanh(self.W(s_prev) + self.U(hidden_states)))
+        e = self.V(tf.nn.tanh(self.W(s_prev_tiled) + self.U(hidden_states)))
         
         # Compute attention weights
         weights = tf.nn.softmax(e, axis=1)
