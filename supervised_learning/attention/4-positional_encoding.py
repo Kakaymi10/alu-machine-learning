5b3801a2500105ba47ede88426247e9230bd8efc
Kakaymi10 <m.moussa@alustudent.com>
2024-10-13 12:18:02 +0200

    pos encoding

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+'''
+Position encoding
+'''
+
+
 import numpy as np
+
 
 def positional_encoding(max_seq_len, dm):
     """Calculates the positional encoding for a transformer."""
@@ -8,7 +14,7 @@ def positional_encoding(max_seq_len, dm):
 
     # Calculate the position indices and dimension indices
     position = np.arange(max_seq_len)[:, np.newaxis]  # Shape: (max_seq_len, 1)
-    div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))  # Shape: (dm/2,)
+    div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))
 
     # Apply sine to even indices (2i) and cosine to odd indices (2i + 1)
     PE[:, 0::2] = np.sin(position * div_term)  # Even dimensions
