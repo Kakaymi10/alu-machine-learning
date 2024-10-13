74d1cce2d0b9011497d01c60c8b593706e84367c
Kakaymi10 <m.moussa@alustudent.com>
2024-10-13 12:02:09 +0200

    Committing

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
-'''
-Module that contains the class RNNDecoder
-'''
 
 
 import tensorflow as tf
-
 SelfAttention = __import__('1-self_attention').SelfAttention
+
 
 class RNNDecoder(tf.keras.layers.Layer):
     def __init__(self, vocab, embedding, units, batch):
         super(RNNDecoder, self).__init__()
-        self.embedding = tf.keras.layers.Embedding(vocab, embedding,
-                                                   embeddings_initializer='glorot_uniform')
-        self.gru = tf.keras.layers.GRU(units,
-                                       return_sequences=True,
-                                       return_state=True,
-                                       recurrent_initializer='glorot_uniform',
-                                       kernel_initializer='glorot_uniform',
-                                       bias_initializer='zeros')
-        self.F = tf.keras.layers.Dense(vocab,
-                                       kernel_initializer='glorot_uniform',
-                                       bias_initializer='zeros')
+
+        # Public instance attributes
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab, output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+        self.F = tf.keras.layers.Dense(vocab)
+        self.units = units
+
+        # Initialize SelfAttention
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
-        # x shape: (batch, 1)
-        # s_prev shape: (batch, units)
-        # hidden_states shape: (batch, input_seq_len, units)
-        
-        x = self.embedding(x)  # (batch, 1, embedding)
-        context, _ = self.attention(s_prev, hidden_states)
-        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
-        
-        output, state = self.gru(x, initial_state=s_prev)
-        
-        output = tf.reshape(output, (-1, output.shape[2]))
+        # 1. Embed the input word
+        x = self.embedding(x)
+
+        # 2. Use Self-Attention to compute context vector
+        context_vector, _ = self.attention(s_prev, hidden_states)
+
+        # 3. Concatenate context vector with the embedded input
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # 4. Pass the concatenated input through the GRU layer
+        output, s = self.gru(x, initial_state=s_prev)
+
+        # 5. Use the Dense layer to generate output probabilities
         y = self.F(output)
-        
-        return y, state
 
+        # Remove the sequence dimension for y
+        y = tf.squeeze(y, axis=1)
 
+        return y, s
