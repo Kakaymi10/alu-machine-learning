d634c56dc6897340750b5efeca40dd8e7b8b7c6e
Kakaymi10 <m.moussa@alustudent.com>
2024-10-19 23:13:18 +0200

    kmeans

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Testing for the optimum number of clusters"""
+"""
+Defines function that tests for the optimum number of clusters by variance
+"""
+
 
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
@@ -7,39 +10,32 @@ variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    """Function that tests for the optimum number of clusters
-    X is a numpy.ndarray of shape (n, d) containing the data set
-    kmin is a positive integer containing the minimum
-        number of clusters to check for (inclusive)
-    kmax is a positive integer containing the maximum
-        number of clusters to check for (inclusive)
-    iterations is a positive integer containing the maximum
-        number of iterations for K-means
-    This function should analyze at least 2 different cluster sizes
     """
-    try:
-        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            return None, None
-        if iterations <= 0 or not isinstance(iterations, int):
-            return None, None
-        if kmax is not None and not isinstance(kmax, int) or kmax <= 0:
-            return None, None
-        if not kmax:
-            kmax = X.shape[0]
-        if not isinstance(kmin, int) or kmin >= kmax or kmin <= 0:
-            return None, None
+    Tests for the optimum number of clusters by variance
 
-        result, df_var = [], []
-        n, d = X.shape
+    parameters:
+        X [numpy.ndarray of shape (n, d)]:
+            contains the dataset used for K-means clustering
+            n: the number of data points
+            d: the number of dimensions for each data point
+        kmin [positive int]:
+            containing the minimum number of clusters to check for (inclusive)
+        kmax [positive int]:
+            containing the maximum number of clusters to check for (inclusive)
+        iterations [positive int]:
+            containing the maximum number of iterations for K-means
 
-        for k in range(kmin, kmax + 1):
-            C, clss = kmeans(X, k, iterations)
-            result.append((C, clss))
-            df_var.append(variance(X, C))
+    function should analyze at least 2 different cluster sizes
 
-        df_var = [df_var[0] - x for x in df_var]
+    should use at most 2 loops
 
-        return result, df_var
-
-    except Exception:
-        return None, None
+    returns:
+        results, d_vars:
+            results [list]:
+                containing the output of K-means for each cluster size
+            d_vars [list]:
+                containing the difference in variance from the smallest cluster
+                    size for each cluster size
+        or None, None on failure
+    """
+    return None, None
