9669821082ad4dd32d33be779f879fc9f8721d06
Kakaymi10 <m.moussa@alustudent.com>
2024-11-02 19:49:27 +0200

    sparse auto encoder

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -2,39 +2,36 @@
 '''sparse autoencoder'''
 
 
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.regularizers import l1
 import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     # Encoder
-    input_layer = Input(shape=(input_dims,))
+    input_layer = keras.Input(shape=(input_dims,))
     encoded = input_layer
     for nodes in hidden_layers:
-        encoded = Dense(nodes, activation='relu')(encoded)
-    encoded = Dense(latent_dims, activation='relu', activity_regularizer=l1(lambtha))(encoded)
+        encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
+    encoded = keras.layers.Dense(latent_dims, activation='relu', activity_regularizer=keras.regularizers.l1(lambtha))(encoded)
     
     # Decoder
     decoded = encoded
     for nodes in reversed(hidden_layers):
-        decoded = Dense(nodes, activation='relu')(decoded)
-    decoded = Dense(input_dims, activation='sigmoid')(decoded)
+        decoded = keras.layers.Dense(nodes, activation='relu')(decoded)
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
     
     # Models
-    encoder = Model(input_layer, encoded)
-    decoder_input = Input(shape=(latent_dims,))
+    encoder = keras.Model(input_layer, encoded)
+    decoder_input = keras.Input(shape=(latent_dims,))
     decoder_layer = decoder_input
     for nodes in reversed(hidden_layers):
-        decoder_layer = Dense(nodes, activation='relu')(decoder_layer)
-    decoder_layer = Dense(input_dims, activation='sigmoid')(decoder_layer)
-    decoder = Model(decoder_input, decoder_layer)
+        decoder_layer = keras.layers.Dense(nodes, activation='relu')(decoder_layer)
+    decoder_layer = keras.layers.Dense(input_dims, activation='sigmoid')(decoder_layer)
+    decoder = keras.Model(decoder_input, decoder_layer)
     
-    auto_input = Input(shape=(input_dims,))
+    auto_input = keras.Input(shape=(input_dims,))
     encoded_auto = encoder(auto_input)
     decoded_auto = decoder(encoded_auto)
-    auto = Model(auto_input, decoded_auto)
+    auto = keras.Model(auto_input, decoded_auto)
     
     auto.compile(optimizer='adam', loss='binary_crossentropy')
     
