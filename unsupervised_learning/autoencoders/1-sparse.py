58466e3244eb46c2f061e59c95e677c0d2bac8d3
Kakaymi10 <m.moussa@alustudent.com>
2024-11-02 20:00:30 +0200

    sparse auto encoder

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -13,8 +13,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     for nodes in hidden_layers:
         encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
     encoded = keras.layers.Dense(
-        latent_dims, 
-        activation='relu', 
+        latent_dims,
+        activation='relu',
         activity_regularizer=keras.regularizers.l1(lambtha)
     )(encoded)
 
@@ -29,8 +29,12 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     decoder_input = keras.Input(shape=(latent_dims,))
     decoder_layer = decoder_input
     for nodes in reversed(hidden_layers):
-        decoder_layer = keras.layers.Dense(nodes, activation='relu')(decoder_layer)
-    decoder_layer = keras.layers.Dense(input_dims, activation='sigmoid')(decoder_layer)
+        decoder_layer = keras.layers.Dense(
+            nodes, activation='relu'
+        )(decoder_layer)
+    decoder_layer = keras.layers.Dense(
+        input_dims, activation='sigmoid'
+    )(decoder_layer)
     decoder = keras.Model(decoder_input, decoder_layer)
 
     auto_input = keras.Input(shape=(input_dims,))
