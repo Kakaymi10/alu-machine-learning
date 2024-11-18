14c944857840c17043f20d23ce94cf3a54562f27
Kakaymi10 <m.moussa@alustudent.com>
2024-11-19 00:09:55 +0200

    policies

--- unsupervised_learning/q_learning_formative/play.py
+++ unsupervised_learning/q_learning_formative/play.py
@@ -1,41 +1,47 @@
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import gymnasium as gym
 import ale_py
+import tensorflow as tf
+import keras
+from keras import layers
+
+# Load the model
+model = keras.models.load_model('policy.h5')
 
 # Register ALE environments (this is not usually necessary if ale_py is installed properly)
 gym.register_envs(ale_py)
 
 # Create the Breakout environment
-env = gym.make('ALE/Breakout-v5')
+env_name = 'ALE/Breakout-v5'
+env = gym.make(env_name)
 
+max_steps_per_episode = 1000
 
-# Load the Q-table from the file
-with open('q_table.pkl', 'rb') as f:
-    q_table = pickle.load(f)
+# Play the game using the trained model
+for episode in range(10):  # Play 10 episodes
+    observation, _ = env.reset()
+    state = np.array(observation)
+    episode_reward = 0
 
-# Play using the loaded Q-table
-num_episodes = 5
-for episode in range(num_episodes):
-    state, info = env.reset()
-    done = False
-    total_reward = 0
+    for timestep in range(1, max_steps_per_episode):
+        env.render()
 
-    while not done:
-        action = np.argmax(q_table[state])  # Choose the best action from Q-table
-        next_state, reward, terminated, truncated, info = env.step(action)
-        done = terminated or truncated
+        # Predict action Q-values
+        state_tensor = tf.convert_to_tensor(state)
+        state_tensor = tf.expand_dims(state_tensor, 0)
+        action_probs = model(state_tensor, training=False)
+        action = tf.argmax(action_probs[0]).numpy()
 
-        state = next_state
-        total_reward += reward
+        # Apply the sampled action in our environment
+        state_next, reward, done, _, _ = env.step(action)
+        state_next = np.array(state_next)
 
-        # Display the game frame
-        plt.imshow(state)
-        plt.axis('off')  # Optional: Hide axes for better visualization
-        plt.pause(0.01)  # Pause to create a frame-by-frame effect
+        episode_reward += reward
+        state = state_next
 
-    print(f"Episode {episode + 1} finished with total reward: {total_reward}")
+        if done:
+            print(f"Episode: {episode + 1}, Reward: {episode_reward}")
+            break
 
 env.close()
-plt.show()  # Display the final frame
